ba4830b84249697c036c4952c5f74f1e9a019e68
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-03-24 11:35:28 -0300

    finalizando backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,26 +2,27 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
-    async index (request, response) {
-        const ongs = await connection('ongs').select('*');
+  async index(request, response) {
+    const ongs = await connection('ongs').select('*');
 
-        return response.json(ongs);
-    },
+    return response.json(ongs);
+  },
 
-    async create (request, response) {
-        const { name, email, whatsapp, city, uf} = request.body;
+  async create(request, response) {
+    const { name, email, password, whatsapp, city, state } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+    const id = crypto.randomBytes(4).toString('HEX');
 
-        await connection('ongs').insert({
-            id,
-            email,
-            name,
-            whatsapp,
-            city,
-            uf,
-        })
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      password,
+      whatsapp,
+      city,
+      state
+    });
 
-        return response.json({ id });
-    }
-};
+    return response.json({ id });
+  }
+}
